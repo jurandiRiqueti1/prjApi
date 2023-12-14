347cf8960b43c95f409ae9dbba9bb18c8f040ab2
JurandiRiquetiJunior <jurandihd4000@gmail.com>
2023-12-14 10:41:37 -0300

    Inicio do server com teste de req e res

--- src/server.js
+++ src/server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const server = express();
+const port = 3000;
+
+server.get("/teste/:id", (req, res) => (
+    res.status(200).json({
+        id: req.params.id,
+        mensagem: `Olá usuário ${req.params.id} seu login foi efetuado!`
+    })
+));
+
+server.listen(port, () => (
+    console.log(`Servidor rodando na porta ${port}`)
+));
