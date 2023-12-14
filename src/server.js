6c733762a93b6cb7d1609537c729a8de51194fc5
JurandiRiquetiJunior <jurandihd4000@gmail.com>
2023-12-14 11:49:40 -0300

    Criação e teste do arquivo .env

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const server = express();
-const port = 3000;
+const port = process.env.PORT;
 
 server.get("/teste/:id", (req, res) => (
     res.status(200).json({
