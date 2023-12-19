fc3f303099da8988ed2878d88405d8361b2f3f9d
Jurandi Riqueti Junior <jurandihd4000@gmail.com>
2023-12-19 00:23:02 -0300

    Adicionado YUP e verificações de entrada

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -4,10 +4,12 @@ import * as yup from 'yup';
 
 interface ICidade{
     nome: string;
+    estado: string;
 };
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
     nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{},{},ICidade>, res: Response) => {
@@ -15,18 +17,26 @@ export const create = async (req: Request<{},{},ICidade>, res: Response) => {
     let validatedData: ICidade | undefined = undefined;
 
     try {
-        validatedData = await bodyValidation.validate(req.body);
+        validatedData = await bodyValidation.validate(req.body, {abortEarly: false});
     } catch (error) {
+        
         const yupError = error as yup.ValidationError;
         
-        return res.json({
-            errors: {
-                default: yupError.message
-            }
+        const validationErros: Record<string, string> = {};
+
+        yupError.inner.forEach(error => {
+
+            if(!error.path) return;
+
+            validationErros[error.path] = error.message;
+        });
+        
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors: validationErros 
         });
     }
 
-    console.log(req.body.nome);
+    console.log(validatedData);
 
     return res.send('Criado');
 };
