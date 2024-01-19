6051211f94cad04f944d3d38dff462ba8caf7e05
JurandiRiquetiJunior <jurandihd4000@gmail.com>
2024-01-19 17:35:27 -0300

    fix: resolvendo problemas no ambiente de produção

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 //import do projeto
+import { Knex } from './server/database/knex';
 import { server } from './server/server'
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
+const startServer = () => {
+    server.listen(port, () => {
+        console.log(`Servidor rodando na porta ${port}`);
+    });
+}
+
+if(process.env.IS_LOCALHOST !== 'true'){
+    Knex.migrate.latest().then(startServer).catch(console.log);
+} else {
+    startServer();
+}
